5dba994a965cd27c8755da6fe8117a1284f3ac4e
Piotr <piotr.zaba@ready4s.com>
2021-06-15 21:05:44 +0200

    feature: added eslint and improved clearCache function

--- projects/angular-query/src/lib/query.service.ts
+++ projects/angular-query/src/lib/query.service.ts
@@ -6,25 +6,22 @@ import { IQueryHTTP, IQueryResult, IQuerySettings } from './types';
 import { prepareParams } from './utils';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QueryService {
   private cache: { [key: string]: any } = {};
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private makeRequest(queryHTTP: IQueryHTTP, subject: BehaviorSubject<IQueryResult>, id?: string): void {
-    this.http.request(
-        queryHTTP.method,
-        queryHTTP.path,
-        {
-          ...queryHTTP?.params ? { params: prepareParams(queryHTTP.params) } : {},
-          ...queryHTTP?.body ? { body: queryHTTP.body } : {},
-          ...queryHTTP.observe ? { observe: queryHTTP.observe } : {},
-          ...queryHTTP.reportProgress ? { reportProgress: queryHTTP.reportProgress }: {},
-          ...queryHTTP.responseType ? { responseType: queryHTTP.responseType } : {},
-        }
-      )
+    this.http
+      .request(queryHTTP.method, queryHTTP.path, {
+        ...(queryHTTP?.params ? { params: prepareParams(queryHTTP.params) } : {}),
+        ...(queryHTTP?.body ? { body: queryHTTP.body } : {}),
+        ...(queryHTTP?.observe ? { observe: queryHTTP.observe } : {}),
+        ...(queryHTTP?.reportProgress ? { reportProgress: queryHTTP.reportProgress } : {}),
+        ...(queryHTTP?.responseType ? { responseType: queryHTTP.responseType } : {}),
+      })
       .subscribe(
         (data: any) => {
           if (id) {
@@ -34,11 +31,15 @@ export class QueryService {
           }
           subject.next({ ...subject.getValue(), ...{ isLoading: false, isRefreshing: false, isSuccess: true, data } });
         },
-        (error: any) => subject.next({ ...subject.getValue(), ...{ isLoading: false, isRefreshing: false, isError: true, error } })
+        (error: any) => subject.next({ ...subject.getValue(), ...{ isLoading: false, isRefreshing: false, isError: true, error } }),
       );
   }
 
-  public query(id: string, queryHTTP: IQueryHTTP, querySettings: Partial<IQuerySettings> = { refreshInBackground: false, saveInCache: false }): Observable<IQueryResult> {
+  public query(
+    id: string,
+    queryHTTP: IQueryHTTP,
+    querySettings: Partial<IQuerySettings> = { refreshInBackground: false, saveInCache: false },
+  ): Observable<IQueryResult> {
     const result: IQueryResult = { isLoading: false, isRefreshing: false, isSuccess: false, isError: false, data: null, error: null };
     let subject: BehaviorSubject<IQueryResult>;
 
@@ -64,13 +65,17 @@ export class QueryService {
   public mutation(queryHTTP: IQueryHTTP): Observable<IQueryResult> {
     const result: IQueryResult = { isLoading: true, isRefreshing: false, isSuccess: false, isError: false, data: null, error: null };
     const subject: BehaviorSubject<IQueryResult> = new BehaviorSubject(result);
-    
+
     this.makeRequest(queryHTTP, subject);
 
     return subject.pipe(takeWhile((data: any) => data.isLoading, true));
   }
 
-  public clearCache(): void {
-    this.cache = {};
+  public clearCache(omitIds: string[] = []): void {
+    Object.keys(this.cache).forEach((id: string) => {
+      if (!omitIds.includes(id)) {
+        delete this.cache[id];
+      }
+    });
   }
 }
--- projects/angular-query/src/lib/types.ts
+++ projects/angular-query/src/lib/types.ts
@@ -1,23 +1,23 @@
 export interface IQuerySettings {
-    refreshInBackground: boolean;
-    saveInCache: boolean;
+  refreshInBackground: boolean;
+  saveInCache: boolean;
 }
-  
+
 export interface IQueryHTTP {
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE';
-    path: string;
-    params?: Record<string, any>;
-    body?: Record<string, any>;
-    reportProgress?: boolean;
-    observe?: 'body' | 'events' | 'response';
-    responseType?: 'arraybuffer' | 'blob' | 'text' | 'json';
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  path: string;
+  params?: Record<string, any>;
+  body?: Record<string, any>;
+  reportProgress?: boolean;
+  observe?: 'body' | 'events' | 'response';
+  responseType?: 'arraybuffer' | 'blob' | 'text' | 'json';
 }
-  
+
 export interface IQueryResult<T = any> {
-    isLoading: boolean;
-    isRefreshing: boolean;
-    isSuccess: boolean;
-    isError: boolean;
-    data: T;
-    error: any;
+  isLoading: boolean;
+  isRefreshing: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  data: T;
+  error: any;
 }
